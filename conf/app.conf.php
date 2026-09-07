7756279086e91b9380fbd6d2d742d4529c95f41e
WorkOfStan <rejthar@stanislavrejthar.com>
2026-09-07 20:40:27 +0200

    feat: add the default Administration of Seablast I18n tables

--- conf/app.conf.php
+++ conf/app.conf.php
@@ -24,5 +24,53 @@ return static function (SeablastConfiguration $SBConfig): void {
         )
         // Seablast::SeablastView uses this class for Latte filter translate
         ->setString(SeablastConstant::TRANSLATE_CLASS, '\Seablast\I18n\SeablastTranslate') // since Seablast v0.2.7
+        // Administration of Seablast I18n tables
+        ->setArrayString(
+            // ON DELETE NO_ACTION prevents risky deletion of children
+            SeablastConstant::ADMIN_TABLE_DELETE_ROW . SeablastConstant::USER_ROLE_EDITOR,
+            [
+                'localised_items', // note: localised_item_types has ON DELETE CASCADE
+            ]
+        )
+        ->setArrayString(
+            SeablastConstant::ADMIN_TABLE_INSERT_ROW . SeablastConstant::USER_ROLE_EDITOR,
+            [
+                'localised_items',
+            ]
+        )        
+        ->setArrayArrayString(
+            SeablastConstant::ADMIN_TABLE_VIEW . SeablastConstant::USER_ROLE_EDITOR,
+            'translations',
+            ['id']
+        )
+        ->setArrayArrayString(
+            SeablastConstant::ADMIN_TABLE_EDIT . SeablastConstant::USER_ROLE_EDITOR,
+            'translations',
+            ['language','translation_key','translation_value']
+        )
+        ->setArrayArrayString(
+            SeablastConstant::ADMIN_TABLE_VIEW . SeablastConstant::USER_ROLE_EDITOR,
+            'localised_items',
+            [
+                'id',
+                //'parent_id',
+                //'created_at','updated_at'
+            ]
+        )
+        ->setArrayArrayString(
+            SeablastConstant::ADMIN_TABLE_EDIT . SeablastConstant::USER_ROLE_EDITOR,
+            'localised_items',
+            [
+                'item_id','language',
+                'item_type_id',
+                'active','title','content',
+                //'friendly_url',
+            ]
+        )
+        ->setArrayArrayString(
+            SeablastConstant::ADMIN_TABLE_VIEW . SeablastConstant::USER_ROLE_EDITOR,
+            'localised_item_types',
+            ['id', 'name']
+        )
     ;
 };
